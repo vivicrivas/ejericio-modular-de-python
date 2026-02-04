4ec219c1b5f80b5a88e1e69b64977e5259e2c52b
Viviana Canales <vivicrivas@gmail.com>
2026-02-04 19:53:12 -0300

    logica de negocio terminada

--- logic/inventory_tool.py
+++ logic/inventory_tool.py
@@ -1,1 +1,19 @@
 # logica de calculo y filtrado
+def get_total_stock(stock_list: list) -> int:
+    """"Calcula el stock total sumando las cantidades en la lista de stock."""
+    total = 0
+    for quantity in stock_list:
+        total = total + quantity  # se abrevia como total += quantity
+        return total
+
+
+def get_low_stock_count(stock_list: list, limit: int) -> int:
+    """"cuenta cuántos productos tienen un stock por debajo del límite dado."""
+    count = 0
+    index = 0
+
+    while index < len(stock_list):
+        if stock_list[index] < limit:
+            count += 1
+        index += 1  # se suma + 1 al index para moverse al siguiente elemento de la lista
+    return count
